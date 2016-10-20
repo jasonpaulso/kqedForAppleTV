dfd250b5d0ab9aaea9b99b40445a83e7626b116d
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-10-20 11:05:04 -0700

    Initial Commit

---
+++ kqed-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kqed-test
+//
+//  Created by Jason Paul Southwell on 10/20/16.
+//  Copyright © 2016 Jason Paul Southwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ kqed-testTests/kqed_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  kqed_testTests.swift
+//  kqed-testTests
+//
+//  Created by Jason Paul Southwell on 10/20/16.
+//  Copyright © 2016 Jason Paul Southwell. All rights reserved.
+//
+
+import XCTest
+@testable import kqed_test
+
+class kqed_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
